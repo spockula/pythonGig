5b3f98ed5ed4107b8903d58a684f069bcdc59e61
spockula <chinedukogu@gmail.com>
2020-02-05 22:12:33 +0100

    update

--- DjangoApi/settings.py
+++ DjangoApi/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -179,4 +178,3 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-django_heroku.settings(locals())
